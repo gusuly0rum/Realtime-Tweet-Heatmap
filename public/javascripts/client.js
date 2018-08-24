ec94350f95d47a82a68b1e0fb6f2af2dbbb1589e
gusuly0rum <donguk.kim1112@gmail.com>
2018-08-24 15:05:40 -0700

    Comment local server connection

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,8 +1,8 @@
-if (process.env.production_mode === 'on') {
-  window.socket = window.io.connect('https://realtime-twitter-feed-heatmap.herokuapp.com/');
-} else {
-  window.socket = window.io.connect('http://localhost:3000');
-}
+// if (process.env.production_mode === 'on') {
+window.socket = window.io.connect('https://realtime-twitter-feed-heatmap.herokuapp.com/');
+// } else {
+//   window.socket = window.io.connect('http://localhost:3000');
+// }
 
 window.socket.on('connection', function() {
   window.socket.emit('begin stream');
